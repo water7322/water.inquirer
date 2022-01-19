dbdd853f897b0d3e19a43741c6f34a40a67dd583
zhangyitao <>
2022-01-19 21:29:23 +0800

    inquirer测试

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
-function init(v: string): void {
-    console.log(v);
-}
+import * as inquirer from 'inquirer';
 
-export { init };
+async function select(content: string, options: Array<{ [key: string]: any }>) {
+    const val = await ask('list', content || '请选择', {
+        choices: options?.map((item) => item.text),
+        askAnswered: true
+    });
+    const item = options.find((item) => item.text === val);
+    return item?.value;
+}
+async function checkbox(content: string, options: Array<{ [key: string]: any }>) {
+    const val: Array<any> = await ask('checkbox', content || '请选择', {
+        choices: options?.map((item) => item.text),
+        askAnswered: true
+    });
+    const map: { [key: string]: any } = {};
+    options.forEach((item) => (map[item.text] = item.value));
+    const items = val?.map((val) => map[val]);
+    return items;
+}
+async function input(content: string) {
+    const val = await ask('input', content || '请输入');
+    return (val || '').trim();
+}
+async function confirm(content: string) {
+    const val = await ask('confirm', content || '请选择');
+    return !!val;
+}
+async function ask(type: string, content: string, param?: object) {
+    const result = await inquirer.prompt([{ type: type, name: 'water', message: content, ...param }]);
+    return result.water;
+}
+export { select, checkbox, input, confirm, ask };
