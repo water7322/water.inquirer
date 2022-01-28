7f7cb0df7ed5e4743d2e52a2d049e1d1413fe962
zhangyitao <>
2022-01-28 16:36:13 +0800

    修改配置

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,20 @@ interface Option {
 
 async function select(content: string, options: Array<Option>) {
     const val = await ask('list', content || '请选择', {
-        choices: options?.map((item) => item.text),
+        choices: options?.map(item => item.text),
         askAnswered: true
     });
-    const item = options.find((item) => item.text === val);
+    const item = options.find(item => item.text === val);
     return item?.value;
 }
 async function checkbox(content: string, options: Array<Option>) {
     const val: Array<any> = await ask('checkbox', content || '请选择', {
-        choices: options?.map((item) => item.text),
+        choices: options?.map(item => item.text),
         askAnswered: true
     });
-    const map: { [key: string]: any } = {};
-    options.forEach((item) => (map[item.text] = item.value));
-    const items = val?.map((val) => map[val]);
+    const map: {[key: string]: any} = {};
+    options.forEach(item => (map[item.text] = item.value));
+    const items = val?.map(val => map[val]);
     return items;
 }
 async function input(content: string) {
@@ -32,7 +32,7 @@ async function confirm(content: string) {
     return !!val;
 }
 async function ask(type: string, content: string, param?: object) {
-    const result = await inquirer.prompt([{ type: type, name: 'water', message: content, ...param }]);
+    const result = await inquirer.prompt([{type: type, name: 'water', message: content, ...param}]);
     return result.water;
 }
-export { select, checkbox, input, confirm, ask };
+export {select, checkbox, input, confirm, ask};
